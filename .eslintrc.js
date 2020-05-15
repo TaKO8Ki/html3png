4e2ca70d94ed9564ec16b0456afd848148f3dd2b
Takayuki Maeda <41065217+TaKO8Ki@users.noreply.github.com>
2020-05-15 23:24:12 +0900

    fix indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
     },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        "ecmaVersion": 11,
-        "sourceType": "module"
+        ecmaVersion: 11,
+        sourceType: "module",
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    plugins: ["react"],
+    rules: {},
 };
